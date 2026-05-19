a503849cb772ed05b398fc06e77a1d8661d41407
SS53SS <urueco2010@gmail.com>
2026-05-19 16:19:54 +0900

    05問題追加

---
+++ questions05.js
@@ -0,0 +1,18 @@
+// questions05.js
+quizDataRaw = [
+    {
+        "text": "TCP/IPネットワークでは、ネットワーク上にコンピュータを識別するために、「　　　　　」を利用する",
+        "choices": ["MACアドレス", "ARPアドレス", "IPアドレス", "DNS名", "ホスト名", "ポート番号"],
+        "correctIndex": 2
+    },
+    {
+        "text": "完全修飾ドメイン名とはどれか",
+        "choices": ["FQDN", "ホスト名", "ドメイン名", "サブドメイン", "IPアドレス"],
+        "correctIndex": 0
+    },
+    {
+        "text": "DNSとは",
+        "choices": ["FQDNとIPアドレスを相互に変換する仕組み", "IPアドレスからMACアドレスを取得する仕組み", "Webページを暗号化して転送する仕組み", "メールを送受信するための仕組み"],
+        "correctIndex": 0
+    }
+];
